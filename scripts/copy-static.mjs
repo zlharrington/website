3bf315914231db3c98460a249482c31b440d0f55
zlharrington <zach@harringtonit.com>
2026-08-17 10:57:57 -0700

    Include redirects in site build

--- scripts/copy-static.mjs
+++ scripts/copy-static.mjs
@@ -4,6 +4,6 @@ await mkdir('dist', { recursive: true });
 await cp('assets', 'dist/assets', { recursive: true });
 await cp('.well-known', 'dist/.well-known', { recursive: true });
 
-for (const file of ['robots.txt', 'sitemap.xml', 'site.webmanifest']) {
+for (const file of ['robots.txt', 'sitemap.xml', 'site.webmanifest', '_redirects']) {
   await copyFile(file, `dist/${file}`);
 }
